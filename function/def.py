8aa3a35f2a7ca7917a0c01d1a1307311fbd4d343
wanglufei <2686221966@qq.com>
2022-08-31 16:18:25 +0800

    python 函数

---
+++ function/def.py
@@ -0,0 +1,11 @@
+from random import randint
+
+def roll_dice(n=2):
+    total = 0
+    for _ in range(n):
+        total += randint(1, 6)
+    return total
+
+
+print(roll_dice())
+print(roll_dice(3))
